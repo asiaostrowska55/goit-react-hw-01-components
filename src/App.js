62acd1241ea3d3101dd04e6250df8e333f3d7a47
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-03-18 21:33:21 +0100

    update task 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import Profile from './components/Profile';
-import { Style } from './components/Style';
-import user from '../user.json';
+import user from './user.json';
+import Profile from 'components/Profile';
 
-function Welcome(user) {
-  return (
-    <div>
-      <h1>Hello, {user.name}. This is a test! </h1>
-      <p> Your age is {user.age}</p>
-      <p> Your favourite color is {user.color}</p>
-    </div>
-  );
-}
+console.log(user);
 
 function App() {
   return (
     <div>
-      <Welcome name="Marcin" age="150" color="black" />
-      {/* <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        /> */}
+      <h2>1-Profil w sieci społecznościowej</h2>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <h2>2- Sekcja "statystyki"</h2>
+      <h2>3 - Lista znajomych</h2>
+      <h2>4 - Historia transakcji</h2>
     </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,32 @@
-// import user from '../user.json';
+import css from '../components/Profile.module.css';
 
-<Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>;
+const Profile = props => {
+  const { username, tag, location, avatar, followers, views, likes } = props;
+  return (
+    <div className={css.profile}>
+      <div className={css.user}>
+        <img className={css.avatar} src={avatar} alt={avatar} width="120" />
+        <p className={css.name}>{username}</p>
+        <p className={css.descr}>@{tag}</p>
+        <p className={css.descr}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li className={css.item}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
