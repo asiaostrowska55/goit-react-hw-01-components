4a619a45a3e141d797dde9612a10d9b972830896
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-03-18 15:04:25 +0100

    przygotowanie plików

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import user from '../user.json';
+
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>;
+
+// const Profile = ({username, tag, location, avatar, stats}) => ();
